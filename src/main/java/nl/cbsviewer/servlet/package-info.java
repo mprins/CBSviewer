17d0ba8aa1a63a4f780fe27206474ced439d550a
Mark Prins <mprins@users.sf.net>
2012-09-15 16:40:36 +0200

    packages voor java onderdelen aanbrengen

---
+++ src/main/java/nl/cbsviewer/servlet/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Servlets voor de CBSviewer.
+ * @since 1.6
+ */
+package nl.cbsviewer.servlet;
+
---
+++ src/main/java/nl/cbsviewer/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * utilities voor de CBSviewer.
+ * @since 1.6
+ */
+package nl.cbsviewer.util;
+
