af03502f371254422b7930366003889f0ee68178
Mark Prins <prinsmc@minlnv.nl>
2012-09-20 11:38:46 +0200

    stub voor de WMS client #3

---
+++ src/main/java/nl/mineleni/cbsviewer/servlet/wms/WMSClientServlet.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package nl.mineleni.cbsviewer.servlet.wms;
+
+import nl.mineleni.cbsviewer.servlet.AbstractBaseServlet;
+
+/**
+ * WMS client voor de applicatie.
+ * 
+ * @author prinsmc
+ * @todo implmentatie
+ */
+public class WMSClientServlet extends AbstractBaseServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4958212343847516071L;
+
+	/**
+	 * 
+	 */
+	public WMSClientServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
