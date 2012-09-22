d9f783beff16461a774b5e7363b9a7c24d317a89
Mark Prins <mprins@users.sf.net>
2012-09-23 00:56:31 +0200

    simple unit tests

---
+++ src/test/js/jasmine/WMSGetFeatureInfo_spec.js
@@ -0,0 +1,11 @@
+
+/**
+ * Jasmine Unit tests voor WMSGetFeatureInfo.
+ * 
+ * @author prinsmc
+ */
+describe('WMSGetFeatureInfo', function() {
+	it('Object type WMSGetFeatureInfo bestaat', function() {
+		expect(WMSGetFeatureInfo).toBeDefined();
+	});
+});
