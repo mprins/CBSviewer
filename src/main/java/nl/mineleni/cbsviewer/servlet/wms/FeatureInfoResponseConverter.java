5dc314b360fa0fcb6c4b0f4254517fa1d28e5c2b
Mark Prins <prinsmc@minlnv.nl>
2012-11-28 13:31:22 +0100

    geen lege tabel tonen
    Als er geen feature info is, geen lege tabel met alleen de
    attribuutnamen maken.

--- src/main/java/nl/mineleni/cbsviewer/servlet/wms/FeatureInfoResponseConverter.java
+++ src/main/java/nl/mineleni/cbsviewer/servlet/wms/FeatureInfoResponseConverter.java
@@ -16,6 +16,8 @@ import java.io.Writer;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import nl.mineleni.cbsviewer.util.LabelsBundle;
+
 import org.geotools.GML;
 import org.geotools.GML.Version;
 import org.geotools.data.simple.SimpleFeatureIterator;
@@ -33,155 +35,166 @@ import org.xml.sax.SAXException;
  */
 public final class FeatureInfoResponseConverter {
 
-    /**
-     * het inputstream Type.
-     * 
-     * @see FeatureInfoResponseConverter#convertToHTMLTable(InputStream, Type,
-     *      String[])
-     */
-    public enum Type {
-        /** het gmltype. */
-        GMLTYPE,
-        /**
-         * het htmltype. Dit doet niet veel...
-         * 
-         * @see FeatureInfoResponseConverter#cleanupHTML(InputStream)
-         */
-        HTMLTYPE;
-    }
+	/**
+	 * het inputstream Type.
+	 * 
+	 * @see FeatureInfoResponseConverter#convertToHTMLTable(InputStream, Type,
+	 *      String[])
+	 */
+	public enum Type {
+		/** het gmltype. */
+		GMLTYPE,
+		/**
+		 * het htmltype. Dit doet niet veel...
+		 * 
+		 * @see FeatureInfoResponseConverter#cleanupHTML(InputStream)
+		 */
+		HTMLTYPE;
+	}
 
-    /** logger. */
-    private static final Logger LOGGER = LoggerFactory
-            .getLogger(FeatureInfoResponseConverter.class);
+	/** logger. */
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(FeatureInfoResponseConverter.class);
 
-    /**
-     * Cleanup html.
-     * 
-     * @param htmlStream
-     *            the html stream
-     * @return the string
-     * @throws IOException
-     *             Signals that an I/O exception has occurred.
-     * @todo implementatie
-     */
-    private static String cleanupHTML(final InputStream htmlStream)
-            throws IOException {
-        LOGGER.warn("unsported feature");
-        // misschien met net.sourceforge.htmlcleaner:htmlcleaner
-        // http://search.maven.org/#artifactdetails%7Cnet.sourceforge.htmlcleaner%7Chtmlcleaner%7C2.2%7Cjar
-        return convertStreamToString(htmlStream);
-    }
+	/** resource bundle. */
+	private static final LabelsBundle _RESOURCES = new LabelsBundle();
 
-    /**
-     * Converteer gml imputstream naar html tabel.
-     * 
-     * @param gmlStream
-     *            the gml stream
-     * @param attributes
-     *            the attributes
-     * @return een html tabel
-     * @throws IOException
-     *             Signals that an I/O exception has occurred.
-     */
-    private static String convertGML(final InputStream gmlStream,
-            final String[] attributes) throws IOException {
-        final StringBuilder sb = new StringBuilder();
-        try {
-            final GML gml = new GML(Version.WFS1_0);
-            final SimpleFeatureIterator iter = gml
-                    .decodeFeatureIterator(gmlStream);
-            // tabel maken
-            sb.append("<table id=\"attribuutTabel\" class=\"attribuutTabel\">");
-            sb.append("<caption>");
-            sb.append("Informatie over de zoeklocatie.");
-            sb.append("</caption>");
-            sb.append("<thead><tr>");
-            for (final String n : attributes) {
-                sb.append("<th scope=\"col\">" + n + "</th>");
-            }
-            sb.append("</tr></thead>");
-            sb.append("<tbody>");
-            int i = 0;
-            while (iter.hasNext()) {
-                sb.append("<tr class=\"" + (((i++ % 2) == 0) ? "" : "even")
-                        + "\">");
-                final SimpleFeature f = iter.next();
-                for (final String n : attributes) {
-                    sb.append("<td>" + f.getAttribute(n) + "</td>");
-                }
-                sb.append("</tr>");
-            }
-            sb.append("</tbody>");
-            sb.append("</table>");
-            LOGGER.debug("Gemaakte HTML tabel:\n" + sb);
-        } catch (ParserConfigurationException | SAXException e) {
-            LOGGER.error("Fout tijdens parsen van GML. ", e);
-        } finally {
-            gmlStream.close();
-        }
-        return sb.toString();
-    }
+	/**
+	 * Cleanup html.
+	 * 
+	 * @param htmlStream
+	 *            the html stream
+	 * @return the string
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 * @todo implementatie
+	 */
+	private static String cleanupHTML(final InputStream htmlStream)
+			throws IOException {
+		LOGGER.warn("unsported feature");
+		// misschien met net.sourceforge.htmlcleaner:htmlcleaner
+		// http://search.maven.org/#artifactdetails%7Cnet.sourceforge.htmlcleaner%7Chtmlcleaner%7C2.2%7Cjar
+		return convertStreamToString(htmlStream);
+	}
 
-    /**
-     * Converteert een stream naar een string.
-     * 
-     * @param is
-     *            de InputStream met data
-     * @return de data als string
-     * @throws IOException
-     *             Signals that an I/O exception has occurred.
-     */
-    private static String convertStreamToString(final InputStream is)
-            throws IOException {
-        if (is != null) {
-            final Writer writer = new StringWriter();
-            final char[] buffer = new char[1024];
-            try {
-                final Reader reader = new BufferedReader(new InputStreamReader(
-                        is, "UTF-8"));
-                int n;
-                while ((n = reader.read(buffer)) != -1) {
-                    writer.write(buffer, 0, n);
-                }
-            } finally {
-                is.close();
-            }
-            return writer.toString();
-        } else {
-            return "";
-        }
-    }
+	/**
+	 * Converteer gml imputstream naar html tabel.
+	 * 
+	 * @param gmlStream
+	 *            the gml stream
+	 * @param attributes
+	 *            the attributes
+	 * @return een html tabel
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	private static String convertGML(final InputStream gmlStream,
+			final String[] attributes) throws IOException {
+		final StringBuilder sb = new StringBuilder();
+		try {
+			final GML gml = new GML(Version.WFS1_0);
+			final SimpleFeatureIterator iter = gml
+					.decodeFeatureIterator(gmlStream);
+			if (iter.hasNext()) {
+				// tabel maken
+				sb.append("<table id=\"attribuutTabel\" class=\"attribuutTabel\">");
+				sb.append("<caption>");
+				sb.append("Informatie over de zoeklocatie.");
+				sb.append("</caption>");
+				sb.append("<thead><tr>");
+				for (final String n : attributes) {
+					sb.append("<th scope=\"col\">" + n + "</th>");
+				}
+				sb.append("</tr></thead>");
+				sb.append("<tbody>");
+				int i = 0;
+				while (iter.hasNext()) {
+					sb.append("<tr class=\"" + (((i++ % 2) == 0) ? "" : "even")
+							+ "\">");
+					final SimpleFeature f = iter.next();
+					for (final String n : attributes) {
+						sb.append("<td>" + f.getAttribute(n) + "</td>");
+					}
+					sb.append("</tr>");
+				}
+				sb.append("</tbody>");
+				sb.append("</table>");
+				iter.close();
+				LOGGER.debug("Gemaakte HTML tabel:\n" + sb);
+			} else {
+				LOGGER.debug("Geen attribuut info voor deze locatie/zoomnivo");
+				return
 
-    /**
-     * Converteer de input naar een html tabel.
-     * 
-     * @param input
-     *            inputstream met de featureinfo response.
-     * @param Type
-     *            het Type conversie
-     * @param attributes
-     *            namen van de feature attributen
-     * @return een html tabel
-     * @throws IOException
-     *             Signals that an I/O exception has occurred.
-     */
-    public static String convertToHTMLTable(final InputStream input,
-            final FeatureInfoResponseConverter.Type type,
-            final String[] attributes) throws IOException {
-        switch (type) {
-        case GMLTYPE:
-            return convertGML(input, attributes);
-        case HTMLTYPE:
-            return cleanupHTML(input);
-        default:
-            return convertStreamToString(input);
-        }
-    }
+				_RESOURCES.getString("KEY_INFO_GEEN_FEATURES");
+			}
+		} catch (ParserConfigurationException | SAXException e) {
+			LOGGER.error("Fout tijdens parsen van GML. ", e);
+		} finally {
+			gmlStream.close();
+		}
+		return sb.toString();
+	}
 
-    /**
-     * private constructor.
-     */
-    private FeatureInfoResponseConverter() {
-        // private constructor voor utility klasse
-    }
+	/**
+	 * Converteert een stream naar een string.
+	 * 
+	 * @param is
+	 *            de InputStream met data
+	 * @return de data als string
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	private static String convertStreamToString(final InputStream is)
+			throws IOException {
+		if (is != null) {
+			final Writer writer = new StringWriter();
+			final char[] buffer = new char[1024];
+			try {
+				final Reader reader = new BufferedReader(new InputStreamReader(
+						is, "UTF-8"));
+				int n;
+				while ((n = reader.read(buffer)) != -1) {
+					writer.write(buffer, 0, n);
+				}
+			} finally {
+				is.close();
+			}
+			return writer.toString();
+		} else {
+			return "";
+		}
+	}
+
+	/**
+	 * Converteer de input naar een html tabel.
+	 * 
+	 * @param input
+	 *            inputstream met de featureinfo response.
+	 * @param Type
+	 *            het Type conversie
+	 * @param attributes
+	 *            namen van de feature attributen
+	 * @return een html tabel
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	public static String convertToHTMLTable(final InputStream input,
+			final FeatureInfoResponseConverter.Type type,
+			final String[] attributes) throws IOException {
+		switch (type) {
+		case GMLTYPE:
+			return convertGML(input, attributes);
+		case HTMLTYPE:
+			return cleanupHTML(input);
+		default:
+			return convertStreamToString(input);
+		}
+	}
+
+	/**
+	 * private constructor.
+	 */
+	private FeatureInfoResponseConverter() {
+		// private constructor voor utility klasse
+	}
 }
