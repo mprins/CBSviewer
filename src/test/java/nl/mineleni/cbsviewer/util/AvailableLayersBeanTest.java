7d5ca795e36fdc9238948b000d8983dc38f76dfe
Mark Prins <prinsmc@minlnv.nl>
2013-01-24 17:22:12 +0100

    opsplitsen unit test en verduidlijking namespaces

--- src/test/java/nl/mineleni/cbsviewer/util/AvailableLayersBeanTest.java
+++ src/test/java/nl/mineleni/cbsviewer/util/AvailableLayersBeanTest.java
@@ -6,37 +6,16 @@
  */
 package nl.mineleni.cbsviewer.util;
 
-import static org.custommonkey.xmlunit.XMLAssert.assertXpathEvaluatesTo;
-import static org.custommonkey.xmlunit.XMLAssert.assertXpathExists;
-import static org.custommonkey.xmlunit.XMLAssert.assertXpathNotExists;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
-
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-
-import org.custommonkey.xmlunit.XMLUnit;
-import org.custommonkey.xmlunit.exceptions.ConfigurationException;
-import org.custommonkey.xmlunit.exceptions.XMLUnitRuntimeException;
-import org.custommonkey.xmlunit.exceptions.XpathException;
-import org.custommonkey.xmlunit.jaxp13.Validator;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 
 /**
- * Test case voor {@link nl.mineleni.cbsviewer.util.AvailableLayersBean} en voor
- * {@link AvailableLayers.xml} en {@link AvailableLayers.xsd}.
+ * Test case voor {@link nl.mineleni.cbsviewer.util.AvailableLayersBean}.
  * 
  * @author prinsmc
  */
@@ -49,46 +28,23 @@ public class AvailableLayersBeanTest {
 	private AvailableLayersBean bean;
 
 	/**
-	 * Converteert een stream naar een string.
-	 * 
-	 * @param is
-	 *            de InputStream met data
-	 * @return de data als string
-	 * @throws IOException
-	 *             Signals that an I/O exception has occurred.
-	 */
-	private static String convertStreamToString(final InputStream is)
-			throws IOException {
-		if (is != null) {
-			final Writer writer = new StringWriter();
-			final char[] buffer = new char[1024];
-			try {
-				final Reader reader = new BufferedReader(new InputStreamReader(
-						is, "UTF-8"));
-				int n;
-				while ((n = reader.read(buffer)) != -1) {
-					writer.write(buffer, 0, n);
-				}
-			} finally {
-				is.close();
-			}
-			return writer.toString();
-		} else {
-			return "";
-		}
-	}
-
-	/**
-	 * set up.
+	 * set up before each test.
 	 * 
 	 * @throws Exception
 	 *             the exception
 	 */
 	@Before
-	public void setUp() throws Exception {
-		XMLUnit.setIgnoreWhitespace(true);
-		XMLUnit.setIgnoreComments(true);
+	public void beforeTest() throws Exception {
 		bean = new AvailableLayersBean();
+	}
+
+	/**
+	 * after each test.
+	 * 
+	 */
+	@After
+	public void afterTest() {
+		bean = null;
 	}
 
 	@Test
@@ -102,104 +58,6 @@ public class AvailableLayersBeanTest {
 		assertFalse(bean.asJSON(false)
 				.startsWith("/* <![CDATA[ */var _layers="));
 		assertTrue(bean.asJSON(false).startsWith("["));
-	}
-
-	/**
-	 * XML testcase voor test {@link AvailableLayers.xml} en
-	 * {@link invalidAvailableLayers.xml}.
-	 */
-	@Test
-	public void testAvailableLayersXML() {
-		// valideer document
-		final Validator v = new Validator();
-		final Source schema = new StreamSource(this.getClass().getClassLoader()
-				.getResourceAsStream("AvailableLayers.xsd"));
-		v.addSchemaSource(schema);
-
-		final Source invaliddoc = new StreamSource(this.getClass()
-				.getClassLoader()
-				.getResourceAsStream("invalidAvailableLayers.xml"));
-		try {
-			v.isInstanceValid(invaliddoc);
-			fail("Expected excepion did not occur.");
-		} catch (final XMLUnitRuntimeException x) {
-			assertEquals("Schema is invalid", x.getMessage());
-		}
-
-		final Source doc = new StreamSource(this.getClass().getClassLoader()
-				.getResourceAsStream("AvailableLayers.xml"));
-		assertTrue(v.isInstanceValid(doc));
-
-		// test inhoud van het geldige test document
-		try {
-			final String TESTXML = convertStreamToString(this.getClass()
-					.getClassLoader()
-					.getResourceAsStream("AvailableLayers.xml"));
-			// vanwege gekkigheid in XPath werken seleciets in de default
-			// namespace niet, bijv.
-			// /Layers/Layerdescriptor[1]/id faalt.
-			// Daarom local-name functie gebruiken
-			assertXpathExists(
-					"/*[local-name()='Layers']/*[local-name()='Layerdescriptor']/*[local-name()='id']",
-					TESTXML);
-
-			assertXpathEvaluatesTo("" + 2,
-					"count(//*[local-name()='Layerdescriptor'])", TESTXML);
-
-			assertXpathEvaluatesTo(
-					ID1,
-					"//*[local-name()='Layerdescriptor'][1]/*[local-name()='id']",
-					TESTXML);
-
-			assertXpathExists(
-					"//*[local-name()='Layerdescriptor'][2]/*[local-name()='link']",
-					TESTXML);
-			assertXpathNotExists(
-					"//*[local-name()='Layerdescriptor'][1]/*[local-name()='link']",
-					TESTXML);
-
-		} catch (ConfigurationException | XpathException | IOException
-				| SAXException e) {
-			e.printStackTrace();
-
-			fail(e.getMessage());
-		}
-	}
-
-	/**
-	 * XML testcase voor {@link AvailableLayers.xsd}.
-	 */
-	@Test
-	public void testAvailableLayersXSD() {
-		try {
-			// valideer xsd
-			final Source schema = new StreamSource(this.getClass()
-					.getClassLoader()
-					.getResourceAsStream("AvailableLayers.xsd"));
-			final Validator v = new Validator();
-			v.addSchemaSource(schema);
-			assertTrue(v.isSchemaValid());
-		} catch (final ConfigurationException e) {
-			fail(e.getMessage());
-		}
-
-	}
-
-	/**
-	 * XML testcase voor deployment {@link AvailableLayers.xml}.
-	 * 
-	 * @todo xpath tests
-	 */
-	@Test
-	public void testDeploymentAvailableLayersXML() {
-		// valideer document
-		final Source schema = new StreamSource(this.getClass().getClassLoader()
-				.getResourceAsStream("AvailableLayers.xsd"));
-		final Source doc = new StreamSource(this.getClass().getClassLoader()
-				.getResourceAsStream("../classes/AvailableLayers.xml"));
-		final Validator v = new Validator();
-		v.addSchemaSource(schema);
-		assertTrue(v.isInstanceValid(doc));
 	}
 
 	/**
