abb9eea3cf5b34dbbc0a2300b1715aefb08b93cb
Mark Prins <prinsmc@minlnv.nl>
2013-02-19 16:58:04 +0100

    Overviewmap voor RIA
    Eerste versie van een overzichtskaartje voor de RIA

--- src/main/webapp/js/config.js
+++ src/main/webapp/js/config.js
@@ -25,7 +25,6 @@ config.featureInfoDiv = "featureinfo";
 /** instellingen voor de kaart. Voorlopig alleen RD projectie. */
 config.map = {
 	projection : "EPSG:28992",
-	theme : null,
 	restrictedExtent : new OpenLayers.Bounds(0, 300000, 280000, 625000),
 	resolutions : [ 3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360,
 			1.680, 0.840, 0.420 ],
